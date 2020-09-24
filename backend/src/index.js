efc35e7ef117fb6d3f19cf3329dfaf74186dff79
chowdha2 <arnab.chowdhury@aalto.fi>
2020-09-24 08:42:53 +0300

    Remove API key

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,11 +7,6 @@ const bodyParser = require('koa-body',)();
 const fetch = require('node-fetch',);
 const cors = require('kcors',);
 
-// const appId = process.env.APPID || '203b57b10848013a65d81ebc3c505efe';
-// const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
-// const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
-// const port = process.env.PORT || 9000;
-
 const appId = process.env.APPID || '';
 const mapURI = process.env.MAP_ENDPOINT || "";
 const targetCity = process.env.TARGET_CITY || "";
